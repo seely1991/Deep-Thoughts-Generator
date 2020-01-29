a4c8141c2bf6ade2ff1c0c9ee84c5c51f9e8039c
seely1991 <40604521+seely1991@users.noreply.github.com>
2020-01-29 16:33:36 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,57 +1,6 @@
-//imports from quotes.js
+//imports from quotes and backgrounds
 import { quotes } from './quotes.js';
-
-//array of backgrounds
-const backgrounds = [
-	{
-		image: 'https://i.giphy.com/media/rAJfdmusNFDDG/giphy.webp',
-		description: 'sunset with fog' 
-	},
-	{
-		image: 'https://media.giphy.com/media/XwoXfc0yayzpm/giphy.gif',
-		description: 'insects swarming around plants at sunset'
-	},
-	{
-		image: 'https://i.giphy.com/media/xUA7b4arnbo3THfzi0/giphy.webp',
-		description: 'fog coming through the forest'
-	},
-	{
-		image: 'https://i.giphy.com/media/4Rob5KCzwlwoU/giphy.webp',
-		description: 'pink sunset over water'
-	},
-	{
-		image: 'https://i.giphy.com/media/EKpmZuydbsmRy/giphy.webp',
-		description: 'bright sun over ocean'
-	},
-	{
-		image: 'https://i.giphy.com/media/pRzlhjFM60A5q/giphy.webp',
-		description: 'stinging nettle'
-	},
-	{
-		image: 'https://i.giphy.com/media/FGlv0Mpp44HS0/giphy.webp',
-		description: 'daisies in the rain'
-	},
-	{
-		image: 'https://media.giphy.com/media/xUA7aS8zKYFHbng6NW/giphy.gif',
-		description: 'stream flowing in a pool of water'
-	},
-	{
-		image: 'https://media3.giphy.com/media/NPP4Z7wMIf0SA/giphy.gif',
-		description: 'waterfall on the beach'
-	},
-	{
-		image: 'https://media.giphy.com/media/WteKbag35EBfq/giphy.gif',
-		description: 'fast moving snow water'
-	},
-	{
-		image: 'https://media.giphy.com/media/l3vQXKPdzpShJtTCE/giphy.gif',
-		description: 'stream water flowing towards camera'
-	},
-	{
-		image: 'https://media.giphy.com/media/gvKru3mU4wLFm/giphy.gif',
-		description: 'snowy woods'
-	}
-]
+import { backgrounds } from './backgrounds.js';
 
 //global variable for switching backgrounds with a crossfade
 let currentBackground = 1;
